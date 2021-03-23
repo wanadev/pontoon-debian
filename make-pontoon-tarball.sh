995fefdf761e6013f4cc033a084e272023d902c8
Fabien LOISON <flo@flogisoft.com>
2021-03-23 18:15:56 +0100

    Initial commit

---
+++ make-pontoon-tarball.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Dependencies: build-essential git python3 python3-dev python3-venv nodejs npm
+
+APP_NAME=pontoon
+VERSION=$1
+test -z "$VERSION" && VERSION=$(date +%Y.%m.%d.0)
+BUILD_DIR=$PWD/build
+OUTPUT_DIR=$PWD/build/$APP_NAME-$VERSION
+OUTPUT_TARBALL=$PWD/dist/${APP_NAME}_$VERSION.tar.gz
+PY37_REQUIREMENTS_IN=$PWD/debian/requirements.py37.in
+DEBIAN_DIR=$PWD/debian
+
+# Exit on errors
+set -e
+
+# Clean build dirs
+rm -rf $BUILD_DIR
+rm -rf $OUTPUT_DIR
+
+# Create directories
+mkdir -p $BUILD_DIR
+mkdir -p $OUTPUT_DIR
+mkdir -p $(dirname $OUTPUT_TARBALL)
+
+# Clone Pontoon
+git clone https://github.com/mozilla/pontoon.git $BUILD_DIR/$APP_NAME.git
+cd $BUILD_DIR/$APP_NAME.git
+
+# Install Python dependencies
+python3 -m venv __env__
+source __env__/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Install Node dependencies
+cd frontend/
+npm install
+cd -
+npm install
+
+# Set some basic config to build Pontoon front
+export SECRET_KEY=pontoonsecret
+export DATABASE_URL=sqlite://:memory:
+export DJANGO_DEBUG=True
+
+# Build the front
+cd frontend/
+npm run build
+cd -
+npm run build
+python3 manage.py collectstatic
+
+# Generates Python 3.7 requirements file
+pip install pip-tools
+cp $PY37_REQUIREMENTS_IN .
+pip-compile --generate-hashes requirements.py37.in
+
+# Leave the virtualenv
+deactivate
+
+# Copy files
+cp -vr static/ $OUTPUT_DIR
+cp -vr pontoon/ $OUTPUT_DIR
+cp -vr assets/ $OUTPUT_DIR
+cp -vr requirements/ $OUTPUT_DIR
+mkdir -vp $OUTPUT_DIR/frontend/
+cp -vr ./frontend/build/ $OUTPUT_DIR/frontend/
+cp -v setup.py $OUTPUT_DIR
+cp -v setup.cfg $OUTPUT_DIR
+cp -v manage.py $OUTPUT_DIR
+cp -v requirements.txt $OUTPUT_DIR
+cp -v requirements.py37.txt $OUTPUT_DIR
+cp -v webpack-stats.json $OUTPUT_DIR
+cp -v LICENSE $OUTPUT_DIR
+cp -v README.md $OUTPUT_DIR
+cp -v contribute.json $OUTPUT_DIR
+cp -v CONTRIBUTING.rst $OUTPUT_DIR
+cp -v CODE_OF_CONDUCT.md $OUTPUT_DIR
+cp -vr $DEBIAN_DIR $OUTPUT_DIR
+
+# Make the tarball
+cd $(dirname $OUTPUT_DIR)
+tar -cvzf $OUTPUT_TARBALL $(basename $OUTPUT_DIR)
