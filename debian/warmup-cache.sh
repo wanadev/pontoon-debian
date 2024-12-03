d9f3e08ed7a4395dfa837f1104832502d116d308
Uriel <imurx@proton.me>
2024-12-03 21:40:35 +0100

    warmup cache too

---
+++ debian/warmup-cache.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+export DOTENV_PATH=/etc/opt/pontoon.env
+export LANG=c
+
+cd /opt/pontoon/current
+source __env__/bin/activate
+
+python manage.py warmup_cache
+
+deactivate
